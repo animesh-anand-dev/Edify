74cdd270a375940fdf5d9407f161e9d4e69187c6
Mansi Sinha <sinhamansi1998@gmail.com>
2022-06-04 16:00:59 +0530

    Added Category selection Activity

---
+++ app/src/main/java/com/edify/app/CategorySelectionActivity.java
@@ -0,0 +1,37 @@
+package com.edify.app;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class CategorySelectionActivity extends AppCompatActivity {
+    CardView teacher, student;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_category_selection);
+        teacher = (CardView) findViewById(R.id.teacher);
+        student= (CardView) findViewById(R.id.student);
+        teacher.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(CategorySelectionActivity.this, TeacherFormActivity.class);
+                startActivity(intent);
+
+            }
+        });
+        student.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(CategorySelectionActivity.this, StudentFormActivity.class);
+                startActivity(intent);
+
+            }
+        });
+    }
+}
