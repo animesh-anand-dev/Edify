d5f9969ac0a6702fa2bb274c7968bb646a9ae54f
Mansi Sinha <sinhamansi1998@gmail.com>
2022-06-07 12:12:42 +0530

    Working on Teacher Form Activity

--- app/src/main/java/com/edify/app/TeacherForm2Activity.java
+++ app/src/main/java/com/edify/app/TeacherForm2Activity.java
@@ -2,13 +2,71 @@ package com.edify.app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.Toast;
+
+import com.google.android.material.textfield.TextInputEditText;
 
 public class TeacherForm2Activity extends AppCompatActivity {
+    private TextInputEditText teach_address , teach_subject , teach_lang , teach_qualification , teach_profession ,teach_about ;
+    public String teach_method_teaching;
+    private Button teach_form2_to_submit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_teacher_form2);
+
+        teach_address =(TextInputEditText) findViewById(R.id.teach_address_input);
+        teach_subject =(TextInputEditText) findViewById(R.id.teach_subject_input);
+        teach_lang =(TextInputEditText) findViewById(R.id.teach_lang_input);
+        teach_qualification =(TextInputEditText) findViewById(R.id.teach_qualification_input);
+        teach_profession =(TextInputEditText) findViewById(R.id.teach_profession_input);
+        teach_about =(TextInputEditText) findViewById(R.id.teach_about_input);
+        teach_form2_to_submit=(Button) findViewById(R.id.teach_form_submit);
+
+        teach_form2_to_submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                Intent teach_to_submit = new Intent(TeacherForm2Activity.this, TeacherFormActivity.class);
+//                teach_to_submit.putExtra("t_address",teach_address.getText().toString());
+//                teach_to_submit.putExtra("t_subject",teach_subject.getText().toString());
+//                teach_to_submit.putExtra("t_lang",teach_lang.getText().toString());
+//                teach_to_submit.putExtra("t_qualification",teach_qualification.getText().toString());
+//                teach_to_submit.putExtra("t_profession",teach_profession.getText().toString());
+//                teach_to_submit.putExtra("t_about",teach_about.getText().toString());
+//                teach_to_submit.putExtra("t_method",teach_method_teaching);
+//                startActivity(teach_to_submit);
+                Toast.makeText(TeacherForm2Activity.this, getIntent().getStringExtra("t_gender"), Toast.LENGTH_SHORT).show();
+
+
+
+
+
+
+
+
+            }
+        });
+
+    }
+    public void onMethodOfTeachingButtonClicked(View view) {
+
+        boolean checked = ((RadioButton) view).isChecked();
+
+        switch(view.getId()) {
+            case R.id.teach_offline_button:
+                if (checked)
+                    teach_method_teaching = "Offline";
+                break;
+            case R.id.teach_online_button:
+                if (checked)
+                    teach_method_teaching = "Online";
+                break;
+        }
     }
 }
--- app/src/main/java/com/edify/app/TeacherFormActivity.java
+++ app/src/main/java/com/edify/app/TeacherFormActivity.java
@@ -6,14 +6,56 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RadioButton;
+
+import com.google.android.material.textfield.TextInputEditText;
 
 public class TeacherFormActivity extends AppCompatActivity {
+    private TextInputEditText teach_name , teach_mob , teach_email , teach_dob , teach_pin_code;
+    public String teach_gender;
+    private Button teach_form1_to_form2;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_teacher_form);
+        teach_name =(TextInputEditText) findViewById(R.id.teach_name_input);
+        teach_mob =(TextInputEditText) findViewById(R.id.teach_mobile_input);
+        teach_email =(TextInputEditText) findViewById(R.id.teach_email_input);
+        teach_dob =(TextInputEditText) findViewById(R.id.teach_dob_input);
+        teach_pin_code =(TextInputEditText) findViewById(R.id.teach_pincode_input);
+        teach_form1_to_form2 =(Button) findViewById(R.id.teach_form2_next);
 
+        teach_form1_to_form2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent teach_to_form2 = new Intent(TeacherFormActivity.this , TeacherForm2Activity.class);
+                teach_to_form2.putExtra("t_name",teach_name.getText().toString());
+                teach_to_form2.putExtra("t_mob",teach_mob.getText().toString());
+                teach_to_form2.putExtra("t_email",teach_email.getText().toString());
+                teach_to_form2.putExtra("t_dob",teach_dob.getText().toString());
+                teach_to_form2.putExtra("t_pin_code",teach_pin_code.getText().toString());
+                teach_to_form2.putExtra("t_gender",teach_gender);
+                startActivity(teach_to_form2);
+            }
+        });
 
+    }
+
+    public void onGenderButtonClicked(View view) {
+
+        boolean checked = ((RadioButton) view).isChecked();
+
+        switch(view.getId()) {
+            case R.id.teach_male_button:
+                if (checked)
+                    teach_gender = "Male";
+                break;
+            case R.id.teach_female_button:
+                if (checked)
+                    teach_gender = "Female";
+                break;
+        }
     }
 }
